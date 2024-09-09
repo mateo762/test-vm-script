faf355b76f4bdbbd0e64cff5aad642fb2bd739b1
Mateo Echeverry Hoyos <71851119+mateo762@users.noreply.github.com>
2024-09-09 16:48:17 +0200

    Update aro_app_deployment.sh

--- aro_app_deployment.sh
+++ aro_app_deployment.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-# Function to log commands and their output
-log_command() {
-    echo "$ $@" >> $logpath
-    eval "$@" 2>&1 | tee -a $logpath
-    echo "" >> $logpath
-}
-
-# Set up logging
-sudo touch /var/log/aro_deployment_log
-sudo chown $USER:$USER /var/log/aro_deployment_log
-logpath=/var/log/aro_deployment_log
-
-echo "Script started" >> $logpath
-echo "" >> $logpath
-
 # Check if both arguments are provided
 if [ $# -ne 5 ]; then
-    log_command echo "Usage: $0 <SPOKE_RG_NAME> <FRONT_DOOR_FQDN> <SP_APP_ID> <SP_PASSWORD> <TENANT_ID>"
+    echo "Usage: $0 <SPOKE_RG_NAME> <FRONT_DOOR_FQDN> <SP_APP_ID> <SP_PASSWORD> <TENANT_ID>"
     exit 1
 fi
 
@@ -28,29 +13,27 @@ SP_APP_ID=$3
 SP_PASSWORD=$4
 TENANT_ID=$5
 
-log_command echo "Logging in using the service principal..."
-log_command az login --service-principal -u $SP_APP_ID -p $SP_PASSWORD --tenant $TENANT_ID
+echo "Logging in using the service principal..."
+az login --service-principal -u $SP_APP_ID -p $SP_PASSWORD --tenant $TENANT_ID
 
-log_command echo "Setting up environment..."
-log_command AROCLUSTER=$(az aro list -g $SPOKE_RG_NAME --query "[0].name" -o tsv)
-log_command LOCATION=$(az aro show -g $SPOKE_RG_NAME -n $AROCLUSTER --query location -o tsv)
-log_command apiServer=$(az aro show -g $SPOKE_RG_NAME -n $AROCLUSTER --query apiserverProfile.url -o tsv)
-log_command webConsole=$(az aro show -g $SPOKE_RG_NAME -n $AROCLUSTER --query consoleProfile.url -o tsv)
-
-log_command echo "ARO Cluster: $AROCLUSTER"
-log_command echo "Location: $LOCATION"
+echo "Setting up environment..."
+AROCLUSTER=$(az aro list -g $SPOKE_RG_NAME --query "[0].name" -o tsv)
+LOCATION=$(az aro show -g $SPOKE_RG_NAME -n $AROCLUSTER --query location -o tsv)
+apiServer=$(az aro show -g $SPOKE_RG_NAME -n $AROCLUSTER --query apiserverProfile.url -o tsv)
+webConsole=$(az aro show -g $SPOKE_RG_NAME -n $AROCLUSTER --query consoleProfile.url -o tsv)
+echo "ARO Cluster: $AROCLUSTER"
+echo "Location: $LOCATION"
 
 # Log in to ARO cluster
-log_command echo "Logging in to ARO cluster..."
-log_command kubeadmin_password=$(az aro list-credentials --name $AROCLUSTER --resource-group $SPOKE_RG_NAME --query kubeadminPassword --output tsv)
-log_command oc login $apiServer -u kubeadmin -p $kubeadmin_password
+echo "Logging in to ARO cluster..."
+kubeadmin_password=$(az aro list-credentials --name $AROCLUSTER --resource-group $SPOKE_RG_NAME --query kubeadminPassword --output tsv)
+oc login $apiServer -u kubeadmin -p $kubeadmin_password
 
-log_command oc new-project contoso
+oc new-project contoso
+oc adm policy add-scc-to-user anyuid -z contoso
 
-log_command oc adm policy add-scc-to-user anyuid -z contoso
-
-log_command echo "Creating Deployment..."
-log_command cat <<EOF | oc apply -f -
+echo "Creating Deployment..."
+cat <<EOF | oc apply -f -
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -83,10 +66,10 @@ spec:
         fsGroup: 0
 EOF
 
-log_command sleep 10
+sleep 10
 
-log_command echo "Creating Service..."
-log_command cat <<EOF | oc apply -f -
+echo "Creating Service..."
+cat <<EOF | oc apply -f -
 apiVersion: v1
 kind: Service
 metadata:
@@ -103,10 +86,10 @@ spec:
   type: ClusterIP
 EOF
 
-log_command sleep 10
+sleep 10
 
-log_command echo "Creating Ingress..."
-log_command cat <<EOF | oc apply -f -
+echo "Creating Ingress..."
+cat <<EOF | oc apply -f -
 apiVersion: networking.k8s.io/v1
 kind: Ingress
 metadata:
@@ -126,4 +109,4 @@ spec:
                   number: 80
 EOF
 
-echo "Script completed" >> $logpath
+echo "Script completed"
